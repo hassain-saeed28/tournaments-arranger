c476034a2d4e2a66fb7ba4957de2da1e66806b3c
Yokozuna59 <u.yokozuna@gmail.com>
2023-04-21 21:27:40 +0300

    feat(backend): create initial spring boot project (#23)
    Signed-off-by: Yokozuna59 <u.yokozuna@gmail.com>

---
+++ backend/src/test/java/com/swe206/group_two/backend/BackendApplicationTests.java
@@ -0,0 +1,11 @@
+package com.swe206.group_two.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
